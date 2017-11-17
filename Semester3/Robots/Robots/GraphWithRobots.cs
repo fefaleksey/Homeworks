bf6a33d0665fd45f13a9bfdb195488f023e329fe
fefaleksey <fefaleksey@mail.ru>
2017-11-17 18:05:21 +0300

    Robots

---
+++ Semester3/Robots/Robots/GraphWithRobots.cs
@@ -0,0 +1,101 @@
+﻿namespace Robots
+{
+    /// <summary>
+    /// Class which realised algorithm, which checked teleportations,
+    /// destroying all robots exist or not
+    /// </summary>
+    public class GraphWithRobots
+    {
+        private int _numberOfVertices;
+        private bool[,] _matrix;
+        private bool[] _robotsPosition;
+        private bool[] _wasVisited;
+
+        /// <param name="numberOfVertices"></param>
+        /// <param name="positions">positions, where robots are there</param>
+        public GraphWithRobots(int numberOfVertices, int[] positions)
+        {
+            _numberOfVertices = numberOfVertices;
+            _matrix = new bool[numberOfVertices, numberOfVertices];
+            
+            for (var i = 0; i < numberOfVertices; i++)
+                for (var j = 0; j < numberOfVertices; j++)
+                {
+                    _matrix[i, j] = false;
+                }
+            
+            _robotsPosition = new bool[numberOfVertices];
+
+            for (var i = 0; i < numberOfVertices; i++)
+            {
+                _robotsPosition[i] = false;
+            }
+            for (var j = 0; j < positions.Length; j++)
+            {
+                _robotsPosition[positions[j]] = true;
+            }
+            _wasVisited = new bool[numberOfVertices];
+            for (var i = 0; i < numberOfVertices; i++)
+            {
+                _wasVisited[i] = false;
+            }
+        }
+
+        /// <summary>
+        /// Connect two verteces
+        /// </summary>
+        public void ConnectVerteces(int first, int second)
+        {
+            _matrix[first, second] = true;
+            _matrix[second, first] = true;
+        }
+
+        /// <summary>
+        /// If this method finds the robot, which doesn't face other robot at teleportations,
+        ///  return false
+        /// </summary>
+        public bool CheckExistOrNot()
+        {
+            for (var i = 0; i < _numberOfVertices; i++)
+            {
+                if (!_wasVisited[i] && _robotsPosition[i])
+                {
+                    var numberConected = 0;
+                    FindConnectedRobots(i, ref numberConected);
+                    if (numberConected == 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void FindConnectedRobots(int vertex, ref int conected)
+        {
+            if (_wasVisited[vertex])
+            {
+                return;
+            }
+
+            if (_robotsPosition[vertex])
+            {
+                conected++;
+            }
+            _wasVisited[vertex] = true;
+            for (var i = 0; i < _numberOfVertices; i++)
+            {
+                if (_matrix[vertex, i])
+                {
+                    for (var j = 0; j < _numberOfVertices; j++)
+                    {
+                        if (_matrix[i, j])
+                        {
+                            FindConnectedRobots(j, ref conected);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Semester3/Robots/RobotsTest/Tests.cs
@@ -0,0 +1,51 @@
+﻿using Xunit;
+using Robots;
+
+namespace RobotsTest
+{
+    public class RobotsTest
+    {
+        private GraphWithRobots _robots;
+        private int[] _position;
+
+        [Fact]
+        public void TrueTest()
+        {
+            _position = new[] {0, 2};
+            _robots = new GraphWithRobots(4, _position);
+            _robots.ConnectVerteces(1, 0);
+            _robots.ConnectVerteces(1, 2);
+            _robots.ConnectVerteces(1, 3);
+            Assert.True(_robots.CheckExistOrNot());
+        }
+
+        [Fact]
+        public void FalseTest()
+        {
+            _position = new[] {1, 2};
+            _robots = new GraphWithRobots(4, _position);
+            _robots.ConnectVerteces(0, 1);
+            _robots.ConnectVerteces(1, 2);
+            _robots.ConnectVerteces(2, 3);
+            _robots.ConnectVerteces(3, 0);
+            Assert.False(_robots.CheckExistOrNot());
+        }
+
+        [Fact]
+        public void BiggerGraphTest()
+        {
+            _position = new[] {2, 4, 7};
+            _robots = new GraphWithRobots(8, _position);
+            _robots.ConnectVerteces(0, 1);
+            _robots.ConnectVerteces(1, 2);
+            _robots.ConnectVerteces(1, 4);
+            _robots.ConnectVerteces(1, 6);
+            _robots.ConnectVerteces(2, 3);
+            _robots.ConnectVerteces(3, 4);
+            _robots.ConnectVerteces(4, 5);
+            _robots.ConnectVerteces(5, 6);
+            _robots.ConnectVerteces(6, 7);
+            Assert.False(_robots.CheckExistOrNot());
+        }
+    }
+}
