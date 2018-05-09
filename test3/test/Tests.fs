fcd45eb6a4c90d358be970ac9eea71a8c60e3049
Aleksey <fefaleksey@gmail.com>
2018-05-09 21:03:11 +0300

    deleted useless code and changed some names

--- test3/test/Tests.fs
+++ test3/test/Tests.fs
@@ -1,14 +1,12 @@
 module Tests
 
 open Program
-open System
 open Xunit
-
 
 [<Fact>]
 let ``rezult sequence`` () =
-    let rez = Seq.take 5 (getRezult())// |> Seq.toList
-    let equal = [1; -2; 3; -4; 5]// |> List.toSeq
+    let rez = Seq.take 5 (getRezult())
+    let equal = [1; -2; 3; -4; 5]
     let bl = rez = (List.toSeq equal)
     Assert.Equal(equal, rez)
     
--- test3/test3/Program.fs
+++ test3/test3/Program.fs
@@ -1,11 +1,7 @@
 ﻿//first sequence
-let seqInfinite _ = Seq.initInfinite (fun index -> if index % 2 = 0 then 1 else -1)
+let firstSeq _ = Seq.initInfinite (fun index -> if index % 2 = 0 then 1 else -1)
 //second(rezult) sequence
 let getRezult _ = 
-    let seq = seqInfinite()
+    let seq = firstSeq()
     Seq.initInfinite (fun index -> (index + 1) * (Seq.item index seq))
 
-let r = getRezult()
-
-printfn "%A" seqInfinite
-printfn "%A" r//(Seq.take 3 rezult())
