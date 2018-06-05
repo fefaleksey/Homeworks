73d39e9bb1e8ce1e7bf1b6b625ac44a0cc6d9fb6
Aleksey <fefaleksey@gmail.com>
2018-06-05 17:53:56 +0300

    added try with

--- Semester4/task8/task8/Program.fs
+++ Semester4/task8/task8/Program.fs
@@ -3,27 +3,40 @@ open System.Text.RegularExpressions
 open System.Net
 open System.IO
 
+type Result = 
+| Data of string
+| Error
+
 let downloadPage (url : string) =
     async { 
-        let req = WebRequest.Create(url)
-        use! response = req.AsyncGetResponse()
-        use stream = response.GetResponseStream()
-        let reader = new StreamReader(stream)
-        let html = reader.ReadToEnd()
-        return html 
+        try
+            let req = WebRequest.Create(url)
+            use! response = req.AsyncGetResponse()
+            use stream = response.GetResponseStream()
+            let reader = new StreamReader(stream)
+            let html = reader.ReadToEnd()
+            return Data(html)
+        with
+        | error -> return Error
     }
             
 let downloadRefPages (url : string) =
     Async.RunSynchronously <|
         async {
             let! mainHtml = downloadPage url
-            let regex = new Regex(@"<a href=""http://.+?"">")
-            let matches = regex.Matches(mainHtml)
-            let references = [|for m in matches -> m.Value.Substring(9, m.Value.Length - 11)|]
-            let tasks = [|for ref in references -> downloadPage ref|]
-            let! results = Async.Parallel tasks 
-            for i in 0..references.Length - 1 do
-                printfn "%s --- %d" references.[i] results.[i].Length 
+            match mainHtml with
+            | Error -> ()
+            | Data(s) ->
+                let regex = new Regex(@"<a href=""http://.+?"">")
+                let matches = regex.Matches(s)
+                let references = [|for m in matches -> m.Value.Substring(9, m.Value.Length - 11)|]
+                let tasks = [|for ref in references -> downloadPage ref|]
+                let! results = Async.Parallel tasks
+                let results' = Array.choose (function
+                    | Error -> None
+                    | Data(s) -> Some(s)) results
+                for i in 0..references.Length - 1 do
+                    printfn "%s --- %d" references.[i] results'.[i].Length 
         }
         
 [<EntryPoint>]
